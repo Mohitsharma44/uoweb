cd2b760ccff523c106a098a69d69d7efaac64d4c
mohitsharma44 <mohitsharma44@gmail.com>
2016-02-01 18:03:27 -0500

    Initial commit

---
+++ content/static-img/static_images.md
@@ -0,0 +1,10 @@
+Title: Static Images
+Date: 2016-02-01 13:50
+Modified: 2016-01-01 13:50
+Category: images
+Tags: UO, images
+Slug: uo-static-images
+Authors: Mohit Sharma
+Summary: Some static images taken by the imaging sensors at NYU CUSP
+
+![test](images/test.jpg "test Image")
---
+++ pelicanconf.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Mohit Sharma'
+SITENAME = u'Urban Observatory'
+SITEURL = ''
+
+PATH = 'content'
+STATIC_PATHS = ['images']
+
+TIMEZONE = 'America/New_York'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Theme
+THEME = "theme/html5-dopetrope/"
+
+# Blogroll
+LINKS = (('What is Urban Observatory', 'http://cusp.nyu.edu/press-release/nyu-cusp-unveils-first-kind-urban-observatory-downtown-brooklyn/'),
+         ('WSJ2050', 'http://www.wsj.com/articles/as-world-crowds-in-cities-become-digital-laboratories-1449850244'),
+         ('NYTimes', 'http://www.nytimes.com/2013/02/24/technology/nyu-center-develops-a-science-of-cities.html'),
+         ('UO Feeds and Timelapses', '#'),)
+
+# Social widget
+#SOCIAL = (('You can add links in your config file', '#'),
+#          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 5
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+# For html5-dopetrope
+COPYRIGHT = "2016 CUSP"
+ABOUT_LINK = "http://cusp.nyu.edu"
+ABOUT_IMAGE = "images/cusp-logo.png"
+SHOW_COPYRIGHT = True
+
+MAIL = ["masoud.ghandehari@nyu.edu", "greg.dobler@nyu.edu", "mohit.sharma@nyu.edu"]
